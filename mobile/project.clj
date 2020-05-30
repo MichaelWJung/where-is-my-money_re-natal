326639607b8e355239d96b665091ba27f9b20041
Michael Jung <MichaelWJung@users.noreply.github.com>
2020-05-30 14:05:16 +0200

    Specify AGPL as license in project.clj

--- mobile/project.clj
+++ mobile/project.clj
@@ -1,12 +1,12 @@
 (defproject where-is-my-money "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
-            :license {:name "Eclipse Public License"
-                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.9.0"]
-                           [org.clojure/clojurescript "1.10.339"]
+            :license {:name "GNU Affero General Public License"
+                      :url  "https://www.gnu.org/licenses/agpl-3.0.en.html"}
+            :dependencies [[org.clojure/clojure "1.10.1"]
+                           [org.clojure/clojurescript "1.10.597"]
                            [reagent "0.8.1" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server cljsjs/create-react-class]]
-[re-frame "0.10.6"]]
+                           [re-frame "0.10.6"]]
             :plugins [[lein-cljsbuild "1.1.4"]
                       [lein-figwheel "0.5.18"]]
             :clean-targets ["target/" "index.ios.js" "index.android.js" #_($PLATFORM_CLEAN$)]
@@ -37,7 +37,7 @@
                                                                      :output-dir    "target/android"
                                                                      :optimizations :none
                                                                      :target :nodejs}}
-#_($DEV_PROFILES$)]}
+                                                     #_($DEV_PROFILES$)]}
                              :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}
                        :prod {:cljsbuild {:builds [
                                                    {:id           "ios"
@@ -60,27 +60,27 @@
                                                                    :optimizations :simple
                                                                    :target :nodejs
                                                                    :closure-defines {"goog.DEBUG" false}}}
-#_($PROD_PROFILES$)]}}
+                                                   #_($PROD_PROFILES$)]}}
                        :advanced {:dependencies [[react-native-externs "0.2.0"]]
                                   :cljsbuild {:builds [
-                                                   {:id           "ios"
-                                                    :source-paths ["src" "env/prod" "../core/src"]
-                                                    :compiler     {:output-to     "index.ios.js"
-                                                                   :main          "env.ios.main"
-                                                                   :output-dir    "target/ios"
-                                                                   :static-fns    true
-                                                                   :optimize-constants true
-                                                                   :optimizations :advanced
-                                                                   :target :nodejs
-                                                                   :closure-defines {"goog.DEBUG" false}}}
-                                                   {:id           "android"
-                                                    :source-paths ["src" "env/prod" "../core/src"]
-                                                    :compiler     {:output-to     "index.android.js"
-                                                                   :main          "env.android.main"
-                                                                   :output-dir    "target/android"
-                                                                   :static-fns    true
-                                                                   :optimize-constants true
-                                                                   :optimizations :advanced
-                                                                   :target :nodejs
-                                                                   :closure-defines {"goog.DEBUG" false}}}
-#_($ADVANCED_PROFILES$)]}}})
+                                                       {:id           "ios"
+                                                        :source-paths ["src" "env/prod" "../core/src"]
+                                                        :compiler     {:output-to     "index.ios.js"
+                                                                       :main          "env.ios.main"
+                                                                       :output-dir    "target/ios"
+                                                                       :static-fns    true
+                                                                       :optimize-constants true
+                                                                       :optimizations :advanced
+                                                                       :target :nodejs
+                                                                       :closure-defines {"goog.DEBUG" false}}}
+                                                       {:id           "android"
+                                                        :source-paths ["src" "env/prod" "../core/src"]
+                                                        :compiler     {:output-to     "index.android.js"
+                                                                       :main          "env.android.main"
+                                                                       :output-dir    "target/android"
+                                                                       :static-fns    true
+                                                                       :optimize-constants true
+                                                                       :optimizations :advanced
+                                                                       :target :nodejs
+                                                                       :closure-defines {"goog.DEBUG" false}}}
+                                                       #_($ADVANCED_PROFILES$)]}}})
